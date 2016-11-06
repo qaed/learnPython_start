c341c7fde746708569666d76302a4f395775dc81
sheng <1103277858@qq.com>
2016-11-06 21:56:15 +0800

    ex30

---
+++ ex29.py
@@ -0,0 +1,29 @@
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:# 执行
+    print("Too many cats! The world is doomed!")
+
+if people > cats:
+    print("Not many cats! The world is saved!")
+
+if people < dogs:
+    print("The world is drooled on!")
+
+if people > dogs:# 执行
+    print("The world is dry!")
+
+
+dogs += 5
+
+if people >= dogs:# 执行
+    print("People are greater than or equal to dogs.")
+
+if people <= dogs:# 执行
+    print("People are less than or equal to dogs.")
+
+
+if people == dogs:# 执行
+    print("People are dogs.")
